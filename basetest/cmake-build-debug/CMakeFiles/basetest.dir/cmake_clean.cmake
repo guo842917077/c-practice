e5d0096936323b0c199c60dca691f5498d12bf56
guojinlong@ren001.com <guojinlong@ren001.com>
2017-07-26 21:10:20 +0800

    a practice project

---
+++ basetest/cmake-build-debug/CMakeFiles/basetest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basetest.dir/main.cpp.o"
+  "basetest.pdb"
+  "basetest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basetest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
